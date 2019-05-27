ede89c0e0dba26c09a0d06740797ed42fe3dbb5e
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-27 08:47:32 -0500

    add index files and media templates

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import DotGrid from './DotGrid';
+import Image from './Image';
+import Layout from './Layout';
+import Navbar from './Navbar';
+import SEO from './SEO';
+
+export { DotGrid, Image, Layout, Navbar, SEO };
---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import GlobalStyles from './GlobalStyles';
+import theme from './theme';
+import media from './media';
+
+export { GlobalStyles, theme, media };
