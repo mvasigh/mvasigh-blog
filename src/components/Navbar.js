bd3e2f0d24761aee624aa9abf2c34302c4a06fab
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-27 09:52:52 -0500

    add basic NavBar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import { media } from '@styles';
+
+const StyledNavbar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+
+  .grow {
+    flex-grow: 1;
+  }
+
+  ${media.desktop`
+    padding-top: ${({ theme }) => theme.spacing.multiple(4)};
+    padding-bottom: ${({ theme }) => theme.spacing.multiple(4)};
+    padding-left: ${({ theme }) => theme.spacing.multiple(8)};
+    padding-right: ${({ theme }) => theme.spacing.multiple(8)};
+  `}
+`;
+
+const Menu = styled.ul`
+  list-style-type: none;
+  li {
+    display: inline-block;
+    font-weight: 900;
+    font-size: 1.2rem;
+    text-transform: uppercase;
+
+    a {
+      text-decoration: none;
+    }
+
+    &:not(:last-child) {
+      margin-right: ${({ theme }) => theme.spacing.multiple(6)};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2.6rem;
+  font-weight: 900;
+`;
 
 const Navbar = ({ siteTitle }) => {
-  return <nav />;
+  return (
+    <StyledNavbar>
+      <Title className="grow">{siteTitle}</Title>
+      <Menu>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="page-2">Page 2</Link>
+        </li>
+        <li>
+          <Link to="page-2">Page 3</Link>
+        </li>
+      </Menu>
+    </StyledNavbar>
+  );
 };
 
 export default Navbar;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,6 +8,9 @@ const theme = {
       black: [0, 0, 15],
       white: [0, 0, 98]
     }
+  },
+  spacing: {
+    unit: 1
   }
 };
 
@@ -16,5 +19,6 @@ theme.palette.text = {
   primary: [...theme.palette.greyscale.black, 1],
   secondary: [...theme.palette.greyscale.black, 0.8]
 };
+theme.spacing.multiple = num => `${num * theme.spacing.unit}rem`;
 
 export default theme;
