59d8c096d78e33fe388045bcc3e3286e52739152
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-27 22:08:10 -0500

    remove unnecessary div

--- src/components/DotGrid.js
+++ src/components/DotGrid.js
@@ -7,30 +7,36 @@ import { toHslString } from '@libs/color';
 const cx = (x, y) => x;
 const cy = (x, y) => y;
 
+const width = (w, h) => 20 / w;
+const height = (w, h) => 20 / h;
+
 const CIRCLE_RADIUS = 600;
 
-const Layer = styled.div`
+const StyledSvg = styled(animated.svg)`
   width: 100vw;
   height: 100vh;
   z-index: -1;
   position: absolute;
   top: 0;
   left: 0;
-  overflow: hidden;
 `;
 
 const DotGrid = ({ theme }) => {
-  console.log(theme);
   const layerRef = useRef();
-  const [dimensions, setDimensions] = useState(null);
   const [props, set] = useSpring(() => ({
-    xy: [0, 0],
-    config: config.stiff
+    from: {
+      opacity: 0
+    },
+    opacity: 0.2,
+    dimensions: [1920, 1080],
+    xy: [0, 0]
   }));
 
   const handleMouseMove = throttle(200, e =>
     set({ xy: [e.clientX, e.clientY] })
   );
+
+  const handleMouseDown = e => {};
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove);
@@ -39,70 +45,61 @@ const DotGrid = ({ theme }) => {
 
   useEffect(() => {
     const { clientWidth, clientHeight } = layerRef.current;
-    setDimensions([clientWidth, clientHeight]);
+    const dimensions = [clientWidth, clientHeight];
+    set({ dimensions });
   }, []);
 
   return (
-    <Layer ref={layerRef}>
-      {dimensions && (
-        <animated.svg
-          width={dimensions[0]}
-          height={dimensions[1]}
-          xmlns="http://www.w3.org/2000/svg"
-          opacity="0.15"
+    <StyledSvg
+      xmlns="http://www.w3.org/2000/svg"
+      opacity={props.opacity}
+      ref={layerRef}
+    >
+      <defs>
+        <filter id="blur">
+          <feGaussianBlur stdDeviation="0.6" />
+        </filter>
+        <filter id="dilate">
+          <feMorphology operator="dilate" radius="1.1" />
+        </filter>
+        <radialGradient id="hoverGradient">
+          <stop offset="10%" stopColor="#bbb" />
+          <stop offset="30%" stopColor="#555" />
+          <stop offset="100%" stopColor="#000" />
+        </radialGradient>
+        <animated.mask id="hoverMask">
+          <animated.circle
+            cx={props.xy.interpolate(cx)}
+            cy={props.xy.interpolate(cy)}
+            r={CIRCLE_RADIUS}
+            fill="url(#hoverGradient)"
+          />
+        </animated.mask>
+        <animated.pattern
+          id="dots"
+          x="0"
+          y="0"
+          width={props.dimensions.interpolate(width)}
+          height={props.dimensions.interpolate(height)}
         >
-          <defs>
-            <filter id="blur">
-              <feGaussianBlur stdDeviation="0.6" />
-            </filter>
-            <filter id="dilate">
-              <feMorphology operator="dilate" radius="1.1" />
-            </filter>
-            <radialGradient id="hoverGradient">
-              <stop offset="10%" stopColor="#bbb" />
-              <stop offset="30%" stopColor="#555" />
-              <stop offset="100%" stopColor="#000" />
-            </radialGradient>
-            <animated.mask id="hoverMask">
-              <animated.circle
-                cx={props.xy.interpolate(cx)}
-                cy={props.xy.interpolate(cy)}
-                r={CIRCLE_RADIUS}
-                fill="url(#hoverGradient)"
-              />
-            </animated.mask>
-            <pattern
-              id="dots"
-              x="0"
-              y="0"
-              width={25 / dimensions[0]}
-              height={25 / dimensions[1]}
-            >
-              <rect
-                width="1.5"
-                height="1.5"
-                x="15"
-                y="15"
-                fill={toHslString(theme.palette.text.primary)}
-              />
-            </pattern>
-          </defs>
           <rect
-            width={dimensions[0]}
-            height={dimensions[1]}
-            fill="url(#dots)"
-            filter="url(#blur)"
+            width="1.5"
+            height="1.5"
+            x="15"
+            y="15"
+            fill={toHslString(theme.palette.text.primary)}
           />
-          <rect
-            width={dimensions[0]}
-            height={dimensions[1]}
-            fill="url(#dots)"
-            mask="url(#hoverMask)"
-            filter="url(#dilate)"
-          />
-        </animated.svg>
-      )}
-    </Layer>
+        </animated.pattern>
+      </defs>
+      <rect width="100%" height="100%" fill="url(#dots)" filter="url(#blur)" />
+      <rect
+        width="100%"
+        height="100%"
+        fill="url(#dots)"
+        mask="url(#hoverMask)"
+        filter="url(#dilate)"
+      />
+    </StyledSvg>
   );
 };
 
