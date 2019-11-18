45dcd9fd7cccc48d990562bb56e0442b31d0f74b
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-11-17 22:12:59 -0600

    show back button in navbar when in a detail page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,33 +9,38 @@ import { Navbar, Footer, Container, Main } from '@components';
 // import DotGrid from '@components/DotGrid';
 import { lightTheme, darkTheme } from '@styles';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children, location }) => {
+  const showBackButton = Boolean(location.pathname.match(/\/.+\/.+/));
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <ThemeProvider theme={darkTheme}>
-        <>
-          <GlobalStyles />
-          <CodeStyles />
-          <Navbar siteTitle={data.site.siteMetadata.title} />
-          {/* <DotGrid /> */}
-          <Main>
-            <Container>{children}</Container>
-          </Main>
-          <Footer />
-        </>
-      </ThemeProvider>
-    )}
-  />
-);
+      `}
+      render={data => (
+        <ThemeProvider theme={darkTheme}>
+          <>
+            <GlobalStyles />
+            <CodeStyles />
+            <Navbar
+              showBackButton={showBackButton}
+              siteTitle={data.site.siteMetadata.title}
+            />
+            <Main>
+              <Container>{children}</Container>
+            </Main>
+            <Footer />
+          </>
+        </ThemeProvider>
+      )}
+    />
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
