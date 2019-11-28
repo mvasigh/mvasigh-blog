fe11a2359387fe1d00069dabf9642fa284d263db
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-11-28 09:08:59 -0600

    add SVG background

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,59 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '@styles/GlobalStyles';
 import CodeStyles from '@styles/Code';
-import { Navbar, Footer, Container, Main } from '@components';
+import { Background, Navbar, Footer, Container, Main } from '@components';
 // import DotGrid from '@components/DotGrid';
 import { darkTheme } from '@styles';
-import { toRgbString } from '@libs/color';
-
-const BackgroundImageSvg = styled.svg`
-  z-index: -1;
-  position: absolute;
-  top: -5vh;
-  left: 50%;
-  transform: translateX(-50%);
-  overflow: hidden;
-  margin: 0 auto;
-  max-width: 800px;
-  max-height: 100vh;
-  height: 100vh;
-  fill: ${({ theme }) => toRgbString(theme.palette.brand.mintCream)};
-  opacity: 0.3;
-`;
-
-const BackgroundImage = () => {
-  // get the triangle
-  const triPoint1 = [200, 60];
-  const triPoint2 = [0, 400];
-  const triPoint3 = [400, 400];
-
-  // get the square
-  const rectX = 500;
-  const rectY = 500;
-  const rectWidth = 320;
-  const rectHeight = 320;
-
-  return (
-    <BackgroundImageSvg viewBox="0 0 900 700">
-      <polygon
-        points={`${[triPoint1, triPoint2, triPoint3]
-          .map(tri => tri.join(','))
-          .join(' ')}`}
-        transform="rotate(15, 400, 400)"
-      />
-      <circle
-        cx="630"
-        cy="500"
-        r="160"
-        transform={`rotate(30, ${rectX + rectWidth / 2}, ${rectY +
-          rectHeight / 2})`}
-      />
-    </BackgroundImageSvg>
-  );
-};
 
 const Layout = ({ children, location }) => {
   const showBackButton = Boolean(location.pathname.match(/\/.+\/.+/));
@@ -71,7 +24,7 @@ const Layout = ({ children, location }) => {
       render={data => (
         <ThemeProvider theme={darkTheme}>
           <>
-            <BackgroundImage />
+            <Background />
             <GlobalStyles />
             <CodeStyles />
             <Navbar
